6c8cb6cf400e00715761e2795d8058793bdee634
Stephan <stephenholzapfel@aol.com>
2021-12-14 21:28:14 +0100

    Home gelöscht, stattdessen landing-page eingefügt

--- src/main/java/com/lehrerkalender/lehrerkalender/controller/MainController.java
+++ src/main/java/com/lehrerkalender/lehrerkalender/controller/MainController.java
@@ -4,22 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
     @Value("${spring.application.name}")
     String appName;
 
-    @GetMapping("/")
-    public String showHome(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
-    }
-
     @GetMapping("/landing-page")
-    public String showLandingPage() {
+    public String showLandingPage(Model model) {
+        model.addAttribute("appName", appName);
         return "landing-page";
     }
-
 }
